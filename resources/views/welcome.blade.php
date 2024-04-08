c7e87453060d1fbe0cc44463d3d8b8dec257889a
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-04-08 13:05:46 +0300

    Hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,6 +55,7 @@
                     </header>
 
                     <main class="mt-6">
+                        Hello from www dir
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
